c0d0ab560f294e3229c166a16963eb4b2023d635
Ed <rijoedi@gmail.com>
2019-07-29 16:20:58 +0900

    Button Delete to Edit Done.

--- src/main/java/com/aoPremura/springBoot/controller/GuestListController.java
+++ src/main/java/com/aoPremura/springBoot/controller/GuestListController.java
@@ -1,9 +1,10 @@
 package com.aoPremura.springBoot.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,43 +14,47 @@ import com.aoPremura.springBoot.repository.GuestRepository;
 
 @Controller
 public class GuestListController {
-	
+
 	@Autowired
 	private GuestRepository guestRepository;
-	
+
 	@RequestMapping(value = "/guestlist")
 	public String guestList(Model model) {
 		Iterable<Guest> guests = guestRepository.findAll();
-	    model.addAttribute("guests", guests);
+		model.addAttribute("guests", guests);
 
-	    return "guestlist";
+		return "guestlist";
 	}
-	
-	@RequestMapping(value = "/delete/{id}")
-	public String delete(@PathVariable("id") long id, Model model) {
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public String delete(@RequestParam Long id) {
 		Guest guest = guestRepository.findById(id)
-			      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
 		guestRepository.delete(guest);
-		
-		//FINDALL WO SURU
-		Iterable<Guest> guests = guestRepository.findAll();
-	    model.addAttribute("guests", guests);
-		
-	    return "guestlist";
-	}
-	
-	@RequestMapping(value = "save", method = RequestMethod.POST)
-	public String save(@RequestParam("name")String name,@RequestParam("email") String email,
-			@RequestParam("tel") String tel, Model model) {
-		
-		Guest newGuest = new Guest(name,email,tel);
-		guestRepository.save(newGuest);
-		
-		Iterable<Guest> guests = guestRepository.findAll();
-	    model.addAttribute("guests", guests);
 
-	    return "guestlist";
+		return "redirect:/guestlist";
 	}
 
+	@RequestMapping(value = "/update")
+	public String updateUser(@RequestParam Long id, Model model) {
+	   Guest guest = guestRepository.findById(id)
+	    		.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	   
+	   	model.addAttribute("idguest", guest.getIdguest());
+	    model.addAttribute("name", guest.getName());
+	    model.addAttribute("email", guest.getEmail());
+	    model.addAttribute("tel", guest.getTel());
+	    
+	    
+        return "guestedit";
+	}
 
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(@Valid Guest guest) {
+		
+			guestRepository.save(guest);
+			
+		return "redirect:/guestlist";
+	}
 }
